734866efb94af2ee59d02f4e556e1402d0f57cee
unbotcomotu <a20213852@pucp.edu.pe>
2024-04-25 19:36:41 -0500

    primer commit

---
+++ src/main/java/com/example/gticslab420213852/GticsLab420213852Application.java
@@ -0,0 +1,13 @@
+package com.example.gticslab420213852;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GticsLab420213852Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GticsLab420213852Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/gticslab420213852/GticsLab420213852ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.gticslab420213852;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GticsLab420213852ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
